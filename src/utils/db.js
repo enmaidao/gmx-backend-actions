9af6aaf61d88b5573aeb84faf9b131d676e90966
call-by <smart.solidity@gmail.com>
2023-09-02 14:06:59 +1000

    Fixed 24h volume null issue

--- src/utils/db.js
+++ src/utils/db.js
@@ -152,7 +152,8 @@ export const getLastPrices = async () => {
 
 export const get24hVolume = async () => {
     const prevTime = Math.floor(Date.now()/1000) - (60*60*24);
-    const query = `SELECT SUM(usd_amount/1e30) as volume FROM volumes WHERE timestamp > ${prevTime}`;
+    const query = `SELECT IFNULL(SUM(usd_amount/1e30), 0) as volume FROM volumes WHERE timestamp > ${prevTime}`;
+    console.log(query)
     return new Promise(resolve => {
         db.all(query, (err, rows) => {
             if (err) console.error(err.message);
